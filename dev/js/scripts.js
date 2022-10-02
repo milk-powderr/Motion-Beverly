b1ffc598827c1cb719c582aeeae8142c7f835466
milk-powderr <beverlyonita13@gmail.com>
2022-10-02 19:54:57 -0400

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,15 @@
 import { gsap } from "gsap";
+
+
+var tl = gsap.timeline( {repeat:-1, repeatDelay: 0.1} );
+ 
+tl
+
+  .to ( '#overlay', { '--color': 'var(--red'})
+  .to ( '#overlay', { '--color': 'var(--orange'})
+  .to ( '#overlay', { '--color': 'var(--yellow'})
+  .to ( '#overlay', { '--color': 'var(--green'})
+  .to ( '#overlay', { '--color': 'var(--bluee'})
+  .to ( '#overlay', { '--color': 'var(--purple'})
+  .to ( '#overlay', { '--color': 'var(--indigo'})
+  .to ( '#overlay', { '--color': 'var(--pink'})
