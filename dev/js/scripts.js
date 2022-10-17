ed37b6282f9136177b4d806a3fcb985e62bff5cf
milk-powderr <beverlyonita13@gmail.com>
2022-10-17 15:53:56 -0400

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,51 +7,30 @@ function heroAnimation(){
     var tl = gsap.timeline();
     tl.from("#first-line",{duration: 1, alpha:0, y:-100})
       .from("#pre-register-btn",{duration: 1, alpha:0, y:50})
+      .from("#fine", {opacity: 0, x: -100, duration: 1})
     return tl;
 }
 
-function fineIdolsAnimation(){
-  var tl = gsap.timeline();
-  tl.from("#fine",{duration: 1, alpha:0, y:-100})
-    .from("#pre-register-btn",{duration: 1, alpha:0, y:-100})
-  return tl;
-}
-
-
-
 var mainTL = gsap.timeline();
 mainTL.add(heroAnimation());
-mainTL.add(fineIdolsAnimation());
-
 var heroSizeNumber = 1;
 let mm = gsap.matchMedia();
 mm.add("(min-width: 768px)", () => {
   heroSizeNumber = 2;
 });
+mm.add("(max-width: 767px)", () => {
+  heroSizeNumber = 1.25;
+});
 
-
-
-let registerBtn = document.querySelector("#pre-register-btn");
+let registerBtn = document.querySelector("#pre-register-btn", "#fine");
 var buttonAnimation = gsap.timeline({paused:true});
-buttonAnimation.to("#pre-register-btn",{duration:1, scale:heroSizeNumber},"goAway")
+buttonAnimation.to("#pre-register-btn",{duration:0.25, scale:heroSizeNumber},"goAway")
 .to("#first-line",{duration: 1, alpha:0, y:50},"goAway")
-
-let logo = document.querySelector("#fine-idols");
-var fineIdolsAnimation = gsap.timeline({paused:true});
-fineIdolsAnimation.to("#fine-idols",{duration: 1, y:0, opacity: 100},"goAway")
-
-
+.to("#fine", {opacity: 100, x: 0, duration: 1})
 
 registerBtn.addEventListener("mouseover",function(){
    buttonAnimation.play();
 })
 registerBtn.addEventListener("mouseout",function(){
     buttonAnimation.reverse();
-})
-
-logo.addEventListener("mouseover",function(){
-  idolsAnimation.play();
-})
-fineLogo.addEventListener("mouseout",function(){
-   fineIdolsAnimation.reverse();
 })
