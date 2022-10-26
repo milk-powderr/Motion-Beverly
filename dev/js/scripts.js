5d8c4c135650264243ade62f72fc7e4939d0a7d6
milk-powderr <beverlyonita13@gmail.com>
2022-10-25 22:10:59 -0400

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,9 +7,9 @@ createButtonTL
   .to("#first-line", { duration: 0.25, alpha: 0, y: 50 }, "trigger")
   .to("#second-line", { duration: 0.25, alpha: 0, y: 50 }, "trigger")
   .to("#create-btn", { duration: 0.25, y: 100, scale: 2}, "trigger")
-  .to("#create-btn i", { duration: 1, rotation: 20, delay: 0, repeat: 1, yoyo: true}, "-=.5")
+  .to("#create-btn i", { duration: 1, rotation: 20, delay: 0, repeat: -1, yoyo: true}, "-=.5")
   .to( "#overlay", { '--color': 'red'})
-
+  .to( "#overlay", { '--color': 'orange'})
   .to( "#overlay", { '--color': 'yellow'})
   .to( "#overlay", { '--color': 'lime'})
   .to( "#overlay", { '--color': 'aqua'})
@@ -34,7 +34,7 @@ function createAnimation() {
   tl.from("#first-line", { duration: 0.25, y: -100 })
     .from("#second-line", { duration: 0.25, y: -100 })
     .from("#create-btn", { duration: 0.25, y: 100, alpha: 0, scale: 2 }, "-=.5")
-    .from("#create-btn i", { duration: 1, rotation: 20, delay: 0, repeat: 1, yoyo: true }, "-=.5");
+    .from("#create-btn i", { duration: 1, rotation: 20, delay: 0, repeat: -1, yoyo: true }, "-=.5");
   return tl;
 }
 
