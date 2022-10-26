6aea5b0b6a763b9e8c4879359357df59bf0ac251
milk-powderr <beverlyonita13@gmail.com>
2022-10-25 21:40:46 -0400

    edits week 3

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,31 +1,42 @@
 import { gsap } from "gsap";
-gsap.set("#create-btn i",{transformOrigin:"center bottom"});
-gsap.from("#create-btn i",{duration: 1, rotation:20, delay:0, repeat:-1, yoyo:true});
 
-var tl = gsap.timeline( {repeat:-1, delay:0} );
- 
-tl
-  .to ( '#overlay', { '--color': 'red'})
-  .to ( '#overlay', { '--color': 'orange'})
-  .to ( '#overlay', { '--color': 'yellow'})
-  .to ( '#overlay', { '--color': 'lime'})
-  .to ( '#overlay', { '--color': 'aqua'})
-  .to ( '#overlay', { '--color': 'lime'})
-  .to ( '#overlay', { '--color': 'yellow'})
-  .to ( '#overlay', { '--color': 'orange'})
-  .to ( '#overlay', { '--color': 'red'})
+gsap.set("#create-btn", { transformOrigin: "center bottom" })
 
-let createBtn = document.querySelector("#create-btn");
-createBtn.addEventListener("mouseover",function(){
-  gsap.to("#create-btn",{duration:1, scale:2});
-  gsap.to("#create-btn i",{duration: 1, rotateY:20});
-  gsap.to("#first-line",{duration: 1, alpha:0, y:50});
-  gsap.to("#second-line",{duration: 1, alpha:0, y:20});
+var createButtonTL = gsap.timeline({ paused: true });
+createButtonTL
+  .to("#first-line", { duration: 0.25, alpha: 0, y: 50 }, "trigger")
+  .to("#second-line", { duration: 0.25, alpha: 0, y: 50 }, "trigger")
+  .to("#create-btn", { duration: 0.25, y: 100, scale: 2}, "trigger")
+  .to("#create-btn i", { duration: 1, rotation: 20, delay: 0, repeat: 1, yoyo: true}, "-=.5")
+  .to( "#overlay", { '--color': 'red'})
+  .to( "#overlay", { '--color': 'orange'})
+  .to( "#overlay", { '--color': 'yellow'})
+  .to( "#overlay", { '--color': 'lime'})
+  .to( "#overlay", { '--color': 'aqua'})
+  .to( "#overlay", { '--color': 'lime'})
+  .to( "#overlay", { '--color': 'yellow'})
+  .to( "#overlay", { '--color': 'orange'})
+  .to( "#overlay", { '--color': 'red'})
+;
+
+var preregisterBtn = document.querySelector("#create-btn");
+
+preregisterBtn.addEventListener("mouseover", function () {
+  createButtonTL.play();
 })
 
-createBtn.addEventListener("mouseout",function(){
-  gsap.to("#create-btn",{duration:1, scale:1});
-  gsap.to("#create-btn i",{duration: 1, rotateY:0});
-  gsap.to("#first-line",{duration: 1, alpha:1, y:0});
-  gsap.to("#second-line",{duration: 1, alpha:1, y:0});
+preregisterBtn.addEventListener("mouseout", function () {
+  createButtonTL.reverse();
 })
+
+function createAnimation() {
+  var tl = gsap.timeline();
+  tl.from("#first-line", { duration: 0.25, y: -100 })
+    .from("#second-line", { duration: 0.25, y: -100 })
+    .from("#create-btn", { duration: 0.25, y: 100, alpha: 0, scale: 2 }, "-=.5")
+    .from("#create-btn i", { duration: 1, rotation: 20, delay: 0, repeat: 1, yoyo: true }, "-=.5");
+  return tl;
+}
+
+var mainTimeline = gsap.timeline();
+mainTimeline.add(createAnimation());
