9e9fa9a252f19f461737025783f3b30e382cb8d6
milk-powderr <beverlyonita13@gmail.com>
2022-10-25 21:53:45 -0400

    editsedits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,7 +9,7 @@ createButtonTL
   .to("#create-btn", { duration: 0.25, y: 100, scale: 2}, "trigger")
   .to("#create-btn i", { duration: 1, rotation: 20, delay: 0, repeat: 1, yoyo: true}, "-=.5")
   .to( "#overlay", { '--color': 'red'})
-  .to( "#overlay", { '--color': 'orange'})
+
   .to( "#overlay", { '--color': 'yellow'})
   .to( "#overlay", { '--color': 'lime'})
   .to( "#overlay", { '--color': 'aqua'})
