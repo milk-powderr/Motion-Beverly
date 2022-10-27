c280748a268c1afa153d9e65113010a3bc4c7d26
milk-powderr <beverlyonita13@gmail.com>
2022-10-27 13:24:30 -0400

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,4 +4,27 @@ import { GSDevTools } from "gsap/GSDevTools";
 
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
+function simplemotion(){
+    var tl =  gsap.timeline()
+
+    return tl;
+}
+
+function patternmotion(){
+    var tl =  gsap.timeline()
+
+    return tl;
+}
+
+function UImotion(){
+    var tl =  gsap.timeline()
+
+    return tl;
+}
+
 GSDevTools.create();
+
+var mainTL = gsap.timeline();
+mainTL.add(simplemotion())
+.add(patternmotion())
+.add(UImotion());
