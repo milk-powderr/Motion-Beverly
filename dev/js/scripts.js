af6fcfdd81eb6f1412957c4d09c3cdae4702c403
milk-powderr <beverlyonita13@gmail.com>
2022-10-31 08:56:55 -0400

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,10 +7,10 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 function simplemotionin(){
     var tl =  gsap.timeline()
-    .to("#Raw",{x:130, scale:0.9, ease: "back.inOut(1)", duration: 1})
-    .to("#Raw_2",{x:130, scale:0.9, alpha:0, ease: "back.inOut(1)", duration: 1}, 0)
-    .to("#Ellipse",{x:130, scale:0.9, ease: "back.inOut(1)", duration: 1}, 0)
-    .to("#Baked",{x:130, scale:0.9, ease: "back.inOut(1)", duration: 1}, 0)
+    .to("#Raw",{x:130, scale:0.9, ease: "back.inOut(1)", duration: 1, transformOrigin: "center"})
+    .to("#Raw_2",{x:130, scale:0.9, alpha:0, ease: "back.inOut(1)", duration: 1, transformOrigin: "center"}, 0)
+    .to("#Ellipse",{x:130, scale:0.9, ease: "back.inOut(1)", duration: 1, transformOrigin: "center"}, 0)
+    .to("#Baked",{x:130, scale:0.9, ease: "back.inOut(1)", duration:1 , transformOrigin: "center"}, 0)
     ;
 
     return tl;
@@ -18,23 +18,36 @@ function simplemotionin(){
 
 function simplemotionout(){
     var tl =  gsap.timeline()
-    .to("#Raw",{x:230, scale:1, alpha:0, ease: "back.inOut(1)", duration: 1})
-    .to("#Ellipse",{x:230, scale:1, alpha:0, ease: "back.inOut(1)", duration: 1}, 0)
-    .to("#Baked",{x:230, scale:1, ease: "back.inOut(1)", duration: 1}, 0)
+    .to("#Raw",{x:230, scale:1, alpha:0, ease: "back.inOut(1)", duration: 1, transformOrigin: "center"})
+    .to("#Ellipse",{x:230, scale:1, alpha:0, ease: "back.inOut(1)", duration: 1, transformOrigin: "center"}, 0)
+    .to("#Baked",{x:230, scale:1, ease: "back.inOut(1)", duration: 1, transformOrigin: "center"}, 0)
     .to("#Rectangle1",{alpha: 0}, 0)
     ;
 
     return tl;
 }
 
-function patternmotion(){
+function patternmotionin(){
     var tl =  gsap.timeline()
-    .to("#Union_6",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, stagger: 0.25})
-    .to("#Union_5",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, stagger: 0.25})
-    .to("#Union_4",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, stagger: 0.25})
-    .to("#Union_3",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, stagger: 0.25})
-    .to("#Union_2",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, stagger: 0.25})
-    .to("#Union_1",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, stagger: 0.25})
+    .to("#Union_6",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, transformOrigin: "center"})
+    .to("#Union_5",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, transformOrigin: "center"}, 0.25)
+    .to("#Union_4",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, transformOrigin: "center"}, 0.35)
+    .to("#Union_3",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, transformOrigin: "center"}, 0.45)
+    .to("#Union_2",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, transformOrigin: "center"}, 0.55)
+    .to("#Union_1",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, transformOrigin: "center"}, 0.65)
+    ;
+
+    return tl;
+}
+
+function patternmotionout(){
+    var tl =  gsap.timeline()
+    .to("#Union_6",{ ease: "back.inOut(2)", duration: 0.8, scale: 1})
+    .to("#Union_5",{ ease: "back.inOut(2)", duration: 0.8, scale: 1}, 0.25)
+    .to("#Union_4",{ ease: "back.inOut(2)", duration: 0.8, scale: 1}, 0.35)
+    .to("#Union_3",{ ease: "back.inOut(2)", duration: 0.8, scale: 1}, 0.45)
+    .to("#Union_2",{ ease: "back.inOut(2)", duration: 0.8, scale: 1}, 0.55)
+    .to("#Union_1",{ ease: "back.inOut(2)", duration: 0.8, scale: 1}, 0.65)
     ;
 
     return tl;
@@ -51,5 +64,6 @@ GSDevTools.create();
 var mainTL = gsap.timeline();
 mainTL.add(simplemotionin())
 .add(simplemotionout())
-.add(patternmotion())
+.add(patternmotionin())
+.add(patternmotionout())
 .add(UImotion());
