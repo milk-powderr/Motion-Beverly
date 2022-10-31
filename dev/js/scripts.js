75ae8f10b4a0414010909f7e6e9aa5c7697eaacd
milk-powderr <beverlyonita13@gmail.com>
2022-10-31 08:10:31 -0400

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,10 +7,10 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 function simplemotionin(){
     var tl =  gsap.timeline()
-    .to("#Raw",{x:130, scale:0.9, ease: "power4.in"})
-    .to("#Raw_2",{x:130, scale:0.9, alpha:0, ease: "power4.in"}, 0)
-    .to("#Ellipse",{x:130, scale:0.9, ease: "power4.in"}, 0)
-    .to("#Baked",{x:130, scale:0.9, ease: "power4.in"}, 0)
+    .to("#Raw",{x:130, scale:0.9, ease: "back.inOut(1)", duration: 1})
+    .to("#Raw_2",{x:130, scale:0.9, alpha:0, ease: "back.inOut(1)", duration: 1}, 0)
+    .to("#Ellipse",{x:130, scale:0.9, ease: "back.inOut(1)", duration: 1}, 0)
+    .to("#Baked",{x:130, scale:0.9, ease: "back.inOut(1)", duration: 1}, 0)
     ;
 
     return tl;
@@ -18,9 +18,9 @@ function simplemotionin(){
 
 function simplemotionout(){
     var tl =  gsap.timeline()
-    .to("#Raw",{x:230, scale:1, alpha:0, ease: "power4.out"})
-    .to("#Ellipse",{x:230, scale:1, alpha:0, ease: "power4.out"}, 0)
-    .to("#Baked",{x:230, scale:1, ease: "power4.out"}, 0)
+    .to("#Raw",{x:230, scale:1, alpha:0, ease: "back.inOut(1)", duration: 1})
+    .to("#Ellipse",{x:230, scale:1, alpha:0, ease: "back.inOut(1)", duration: 1}, 0)
+    .to("#Baked",{x:230, scale:1, ease: "back.inOut(1)", duration: 1}, 0)
     .to("#Rectangle1",{alpha: 0}, 0)
     ;
 
@@ -29,6 +29,13 @@ function simplemotionout(){
 
 function patternmotion(){
     var tl =  gsap.timeline()
+    .to("#Union_6",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, stagger: 0.25})
+    .to("#Union_5",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, stagger: 0.25})
+    .to("#Union_4",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, stagger: 0.25})
+    .to("#Union_3",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, stagger: 0.25})
+    .to("#Union_2",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, stagger: 0.25})
+    .to("#Union_1",{ ease: "back.inOut(2)", duration: 0.8, scale: 0.7, stagger: 0.25})
+    ;
 
     return tl;
 }
