f779b2db49b8c42bb83396bfd5718abee2acfa60
milk-powderr <beverlyonita13@gmail.com>
2022-11-12 23:04:58 -0500

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,23 +1,31 @@
 import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
 
-gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+gsap.registerPlugin(DrawSVGPlugin, MotionPathPlugin, MotionPathHelper, GSDevTools);
 
 
 function clockmotionspin1(){
     var tl =  gsap.timeline()
-    .to("#Longhand",{ ease: "Power4.easeOut", duration: 1, transformOrigin: "top", rotate: 539.5}, 0)
-    .to("#Shorthand",{ ease: "Power4.easeOut", duration: 1, transformOrigin: "top", rotate: 209, stagger:0.25}, 0)
+    .to("#Longhand",{ ease: "Power2.easeOut", duration: 1, transformOrigin: "top", rotate: 539.5}, 0)
+    .to("#Shorthand",{ ease: "Power2.easeOut", duration: 1, transformOrigin: "top", rotate: 209}, 0)
     ;
     return tl;
 }
 
 function clockmotionspin2(){
     var tl =  gsap.timeline()
-    .to("#Longhand",{ ease: "Power4.easeOut", duration: 1, transformOrigin: "top", rotate: 899}, 0)
-    .to("#Shorthand",{ ease: "Power4.easeOut", duration: 1, transformOrigin: "top", rotate: 238}, 0)
+    .to("#Longhand",{ ease: "Power2.easeOut", duration: 1, transformOrigin: "top", rotate: 899}, 0)
+    .to("#Shorthand",{ ease: "Power2.easeOut", duration: 1, transformOrigin: "top", rotate: 238}, 0)
     ;
+    return tl;
+}
+
+function lastmove(){
+    var tl = gsap.timeline();
+    tl.to("#R12",{duration:3, motionPath:{path:"#12m", align:"#12m", autoRotate:true}, transformOrigin:"center", rotate:45, ease: "expo.in"});
     return tl;
 }
 
@@ -26,4 +34,5 @@ GSDevTools.create();
 var mainTL = gsap.timeline();
 mainTL.add(clockmotionspin1())
 mainTL.add(clockmotionspin2())
+mainTL.add(lastmove())
 ;
