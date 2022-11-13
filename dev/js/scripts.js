9ffb1583b377f9560c78f256b5119635907c4d00
milk-powderr <beverlyonita13@gmail.com>
2022-11-12 19:43:31 -0500

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,15 +8,15 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 function clockmotionspin1(){
     var tl =  gsap.timeline()
     .to("#Longhand",{ ease: "Power4.easeOut", duration: 1, transformOrigin: "top", rotate: 539.5}, 0)
-    .to("#Shorthand",{ ease: "Power4.easeOut", duration: 1, transformOrigin: "top", rotate: 209}, 0)
+    .to("#Shorthand",{ ease: "Power4.easeOut", duration: 1, transformOrigin: "top", rotate: 209, stagger:0.25}, 0)
     ;
     return tl;
 }
 
 function clockmotionspin2(){
     var tl =  gsap.timeline()
-    .to("#Longhand",{ ease: "back.Out(1)", duration: 0.8, transformOrigin: "top", rotate: 899}, 0)
-    .to("#Shorthand",{ ease: "back.Out(1)", duration: 0.8, transformOrigin: "top", rotate: 238}, 0)
+    .to("#Longhand",{ ease: "Power4.easeOut", duration: 1, transformOrigin: "top", rotate: 899}, 0)
+    .to("#Shorthand",{ ease: "Power4.easeOut", duration: 1, transformOrigin: "top", rotate: 238}, 0)
     ;
     return tl;
 }
