d10424ff082bace54a5903fd5dffce13489c6d77
milk-powderr <beverlyonita13@gmail.com>
2022-11-14 16:17:31 -0500

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,34 +6,18 @@ import { MotionPathHelper } from "gsap/MotionPathHelper";
 
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MotionPathPlugin, MotionPathHelper);
 
-function clockmotionspin1(){
+function Bmotion(){
+
+    
     var tl =  gsap.timeline()
-    tl.to("#Longhand",{ ease: "Power2.easeOut", duration: 1, transformOrigin: "top", rotate: 539.5}, 0)
-    .to("#Shorthand",{ ease: "Power2.easeOut", duration: 1, transformOrigin: "top", rotate: 209}, 0)
+    tl.to("#B",{ ease: "Power2.easeOut", duration: 1, transformOrigin: "center"}, 0)
+
     ;
     return tl;
 }
-
-function clockmotionspin2(){
-    var tl =  gsap.timeline()
-    tl.to("#Longhand",{ ease: "Power2.easeOut", duration: 1, transformOrigin: "top", rotate: 899}, 0)
-    .to("#Shorthand",{ ease: "Power2.easeOut", duration: 1, transformOrigin: "top", rotate: 238}, 0)
-    ;
-    return tl;
-}
-
-function lastmove(){
-    var tl = gsap.timeline()
-    tl.to("#R12",{duration:3, motionPath:{path:"#12m", align:"#12m", autoRotate:true}, transformOrigin:"center", rotate:45, ease: "expo.in"});
-    return tl;
-}
-
-var mainTL = gsap.timeline();
-mainTL.add(clockmotionspin1())
-    .add(clockmotionspin2())
-    .add(lastmove())
-;
 
 GSDevTools.create();
 
-MotionPathHelper.create("#12m");
+var mainTL = gsap.timeline();
+mainTL.add(Bmotion())
+;
