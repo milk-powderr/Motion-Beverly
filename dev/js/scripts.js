9f536d0d2c4054d9c2c67a5cef2de4306ad520cd
milk-powderr <beverlyonita13@gmail.com>
2022-11-14 17:13:26 -0500

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,9 +8,23 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MotionPathPlugin, MotionPathHelpe
 
 function Bmotion(){
 
+    gsap.set("#Flower", {scale: 0, transformOrigin: "center"})
+    gsap.set("#Leaf", {scale: 0, transformOrigin: "center"})
+
     var tl =  gsap.timeline()
     tl.from("#B",{ ease: "Power2.easeOut", duration: 1, transformOrigin: "center", x:"-=200", alpha: 0}, 0)
-    tl.from("#Flower",{ ease: "elastic.out(1, 0.3)", duration: 1, transformOrigin: "center", y:"-=100", alpha: 0}, 0.5)
+    tl.from("#Flower",{ ease: "Power2.easeOut", duration: 1.5, transformOrigin: "center", rotate: 100}, 0.5)
+    tl.to("#Flower",{ ease: "elastic.out(1, 0.3)", duration: 1.5, transformOrigin: "center", scale: 1, rotate: 100}, 0.5)
+    tl.to("#Leaf",{ ease: "elastic.out(1, 0.8)", duration: 1, transformOrigin: "center", scale: 1})
+    tl.to("#Leaf",{transformOrigin: "center", yoyo: true, repeat: 1, rotation: 20, duration: 0.3})
+    ;
+    return tl;
+}
+
+function Leafmotion(){
+
+    var tl =  gsap.timeline()
+
     ;
     return tl;
 }
@@ -19,4 +33,6 @@ function Bmotion(){
 GSDevTools.create();
 
 var mainTL = gsap.timeline();
+mainTL.add(Bmotion())
+.add(Leafmotion())
 ;
