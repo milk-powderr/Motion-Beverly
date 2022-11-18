27eab43c0cacc279f19442091be237956893bb0c
milk-powderr <beverlyonita13@gmail.com>
2022-11-18 01:32:54 -0500

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,9 +10,11 @@ function oreozoomout(){
 
     var tl =  gsap.timeline()
 
-    gsap.set(".demo", { attr:{viewBox:"779 453 475.72 276"}, transformOrigin: "center"})
+    gsap.set(".demo", { attr:{viewBox:"748 453 475.72 246"}, transformOrigin: "center"})
+    gsap.set("#Row3", { alpha: 0})
 
-    tl.to(".demo",{ attr:{viewBox:"0 0 1728 1117"}, transformOrigin: "center", delay: 2}, 0)
+    tl.to(".demo",{ attr:{viewBox:"0 0 1728 1117"}, transformOrigin: "center", duration: 1.5, delay: 2, ease: "power3.out"}, 0)
+    .to("#Row3",{ alpha: 1, duration: 0.1}, 0)
     ;
     return tl;
 }
