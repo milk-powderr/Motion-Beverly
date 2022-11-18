850ba680503266f35f4d3905a0e51a375693245a
milk-powderr <beverlyonita13@gmail.com>
2022-11-17 23:42:18 -0500

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -32,8 +32,8 @@ function lastmove(){
     .to("#R9",{ rotate: 0.4, ease: "Power3.easeOut", duration: 1.8, motionPath:{path:"#m9", align:"#m9"}, transformOrigin:"bottom", height: 52}, 0)
     .to("#R10",{ rotate: 90, ease: "Power3.easeOut", duration: 1.8, motionPath:{path:"#m10", align:"#m10"}, transformOrigin:"center"}, 0)
     .to("#R11",{ rotate: 181, ease: "Power3.easeOut", duration: 1.8, motionPath:{path:"#m11", align:"#m11"}, transformOrigin:"center"}, 0)
-    .to("#Longhand",{ rotate: 1000, ease: "Power3.easeOut", duration: 1, motionPath:{path:"#mhands", align:"#mhands"}, transformOrigin:"top", height: 30}, 0)
-    .to("#Shorthand",{ rotate: 438, ease: "Power3.easeOut", duration: 1, motionPath:{path:"#mhands", align:"#mhands"}, transformOrigin:"top"}, 0)
+    .to("#Longhand",{ rotate: 1300, ease: "Power3.easeOut", duration: 1.8, motionPath:{path:"#mhands", align:"#mhands"}, transformOrigin:"top", height: 30}, 0)
+    .to("#Shorthand",{ rotate: 830, ease: "Power3.easeOut", duration: 1.8, motionPath:{path:"#mhands", align:"#mhands"}, transformOrigin:"top", height: 20}, 0)
     ;
     return tl;
 }
