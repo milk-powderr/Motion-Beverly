d26ea9556a8d952cd3e563fa63a4ec97e3457f83
milk-powderr <beverlyonita13@gmail.com>
2022-11-18 01:41:49 -0500

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -13,8 +13,13 @@ function oreozoomout(){
     gsap.set(".demo", { attr:{viewBox:"748 453 475.72 246"}, transformOrigin: "center"})
     gsap.set("#Row3", { alpha: 0})
 
-    tl.to(".demo",{ attr:{viewBox:"0 0 1728 1117"}, transformOrigin: "center", duration: 1.5, delay: 2, ease: "power3.out"}, 0)
+    tl.to(".demo",{ attr:{viewBox:"0 0 1553.19 952"}, transformOrigin: "center", duration: 1.5, delay: 1.5, ease: "power3.out"}, 0)
     .to("#Row3",{ alpha: 1, duration: 0.1}, 0)
+    .to("#Row1",{ x:"-=190", duration: 4, delay: 1.5}, 0)
+    .to("#Row2",{ x:"+=190", duration: 4, delay: 1.5}, 0)
+    .to("#Row3",{ x:"-=190", duration: 4, delay: 1.5}, 0)
+    .to("#Row4",{ x:"+=190", duration: 4, delay: 1.5}, 0)
+    .to("#Row5",{ x:"-=190", duration: 4, delay: 1.5}, 0)
     ;
     return tl;
 }
