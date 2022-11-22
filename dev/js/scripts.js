5515b7cdceed4eac45f92404653b1a3cf6056b16
milk-powderr <beverlyonita13@gmail.com>
2022-11-22 12:44:29 -0500

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -17,7 +17,7 @@ function oreozoomout(){
     gsap.set("#wonderfilled", { transformOrigin:"center", x:"-=106", alpha: 0})
     ;
 
-    tl.to(".demo",{ attr:{viewBox:"0 0 1553.19 900"}, transformOrigin: "center", duration: 1.5, delay: 1.2, ease: "power3.out"}, 0)
+    tl.to(".demo",{ attr:{viewBox:"0 0 1553.19 1000"}, transformOrigin: "center", duration: 1.5, delay: 1.2, ease: "power3.out"}, 0)
     .to("#Row4",{ alpha: 1, duration: 0.2, delay: 0.2}, 0)
     .to("#Row1",{ x:"-=190", duration: 7, delay: 1.5}, 0)
     .to("#Row2",{ x:"+=190", duration: 7, delay: 1.5}, 0)
@@ -35,7 +35,7 @@ function oreozoomout(){
     .to("#words",{ x:"-=1150", duration: 0.8, delay: 5.4, ease: "power3.out"}, 0)
     .to("#Cookieroll",{ rotate: -360.5, duration: 1, ease: "power3.out", motionPath:{path:"#roll", align:"#roll"}, transformOrigin:"center", delay: 5.4, scale: 2.5,onComplete:test()}, 0)
     .to(".up1",{ y:"-=270", duration: 0.7, delay: 5.4, ease: "power3.out"}, 0)
-    .to(".up2",{ y:"+=300", duration: 0.7, delay: 5.4, ease: "power3.out"}, 0)
+    .to(".up2",{ y:"+=360", duration: 0.7, delay: 5.4, ease: "power3.out"}, 0)
     .to("#wonderfilled",{ scale: 3, duration: 0.7, delay: 6.1,  ease: "elastic.out(1, 0.3)", alpha: 1}, 0);
     return tl;
 }
